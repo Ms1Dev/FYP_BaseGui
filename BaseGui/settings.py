c263ebc6ef184197a73babd8aabe0417123cfe86
Michael Swann <michaelswann.se@gmail.com>
2024-02-03 12:12:06 +0000

    Added round spinner

--- BaseGui/settings.py
+++ BaseGui/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'BaseGuiApp',
+    'jquery',
     'bootstrap5'
 ]
 
--- BaseGuiApp/static/js/app.js
+++ BaseGuiApp/static/js/app.js
@@ -7,3 +7,10 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+
+$("#azimuth-slider").roundSlider({
+    radius: 85,
+    sliderType: "default",
+    value: 30
+});
