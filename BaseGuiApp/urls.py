0cf42f415f36a29e331fc5c8242c751ee2fb0a48
Michael Swann <michaelswann.se@gmail.com>
2024-04-22 21:32:01 +0100

    Added login

--- BaseGuiApp/urls.py
+++ BaseGuiApp/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 urlpatterns=[
     path('', views.Index, name="index"),
-    path('index', views.Index, name="index")
+    path('index', views.Index, name="index"),
+    path('log-out', views.Logout, name="log-out"),
+    path('user-login', views.Login, name="user-login")
 ]
--- BaseGuiApp/views.py
+++ BaseGuiApp/views.py
@@ -1,5 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from .forms import LoginForm
 
 
+
+@login_required(login_url="user-login")
 def Index(request):
     return render(request, 'index.html')
+
+
+def Login(request):
+    form = LoginForm()
+
+    context={ 'form': form }
+
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        context["form"] = form
+
+        _username = request.POST.get('username', False)
+        _password = request.POST.get('password', False)
+        
+        user = authenticate(request, username=_username, password=_password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        
+    return render(request, 'login.html', context)
+
+
+def Logout(request):
+    logout(request)
+    return redirect('user-login')
